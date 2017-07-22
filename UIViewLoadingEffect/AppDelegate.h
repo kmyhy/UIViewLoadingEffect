595a74e7ece2b6123d3a961dbc65ddd020e077da
kmyhy <kmyhy@126.com>
2017-07-22 11:18:24 +0800

    Initial Commit

---
+++ UIViewLoadingEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewLoadingEffect
+//
+//  Created by qq on 2017/7/22.
+//  Copyright © 2017年 qq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
